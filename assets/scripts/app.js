b45db6999fb519bfcbe0001cd90e88f8df552525
Ali Amini <aliamini.codes@gmail.com>
2025-03-26 00:29:00 +0430

    To avoid spelling mistakes, (in my code) I only assigned a constant value to each of the choices.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,17 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+
+
+
 const getPlayerChoice = function () {
    const selection = prompt('Rock, Paper or Scissors?', '');
    if (
-    selection !== 'rock' &&
-    selection !== 'paper' &&
-    selection !== 'scissors'
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
   ) {
     alert(`Invalid choice! We chose Rock for you!`);
     return 'rock';
