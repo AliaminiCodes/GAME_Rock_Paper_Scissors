cf1aa0867b617dee37623a08f24387018c09de2f
Ali Amini <aliamini.codes@gmail.com>
2025-03-26 00:21:19 +0430

    I added getPlayerChoice

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const getPlayerChoice = function () {
+   const selection = prompt('Rock, Paper or Scissors?', '');
+   if (
+    selection !== 'rock' &&
+    selection !== 'paper' &&
+    selection !== 'scissors'
+  ) {
+    alert(`Invalid choice! We chose Rock for you!`);
+    return 'rock';
+  }
+}
+
+
+startGameBtn.addEventListener('click', function () {
+  console.log('Game is starting...');
+});
+
